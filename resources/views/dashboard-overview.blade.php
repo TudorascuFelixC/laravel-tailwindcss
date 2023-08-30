d2d7e6abd7beb57202f53dfafff44929276ea422
Alberto Mafla Prado <79513200+A0M0305@users.noreply.github.com>
2023-08-30 15:32:21 +0100

    Sorted Header and PageHeading

--- resources/views/dashboard-overview.blade.php
+++ resources/views/dashboard-overview.blade.php
@@ -87,48 +87,45 @@
     </div>
     <!-------------------- Sidebar END ---------------------->
 
-<!---------------------2nd part------------------------------->
-<div1 class="flex flex-col w-full"> 
-        
+    <!-------------------- Header ---------------------->
+    <div1 class="flex flex-col w-full">     
         <div class="flex flex-row justify-end items-end px-4 mt-4"> 
             <!-- Question Mark Icon -->
-            <div class="pr-2">
-            <div class="border-2 border-black rounded-full w-12 h-12 p-2 pl-2 pr-2 flex items-center justify-center  dark:text-black">
-                <svg class="w-8 h-8 dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7.529 7.988a2.502 2.502 0 0 1 5 .191A2.441 2.441 0 0 1 10 10.582V12m-.01 3.008H10M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-                </svg>
-            </div></div>
+            <div class="pr-5">
+                <div class="border-2 border-black rounded-full w-12 h-12 p-2 pl-2 pr-2 flex items-center justify-center  dark:text-black">
+                    <svg class="w-8 h-8 dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7.529 7.988a2.502 2.502 0 0 1 5 .191A2.441 2.441 0 0 1 10 10.582V12m-.01 3.008H10M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                    </svg>
+                </div>
+            </div>
 
-                <!-- Bell Icon -->
+            <!-- Bell Icon -->
             <div class="border-2 border-black rounded-full w-12 h-12 p-2 pl-2 flex items-center justify-center  dark:text-black">
                 <svg class="w-8 h-8  dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M15.133 10.632v-1.8a5.407 5.407 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V1.1a1 1 0 0 0-2 0v2.364a.944.944 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C4.867 13.018 3 13.614 3 14.807 3 15.4 3 16 3.538 16h12.924C17 16 17 15.4 17 14.807c0-1.193-1.867-1.789-1.867-4.175Zm-13.267-.8a1 1 0 0 1-1-1 9.424 9.424 0 0 1 2.517-6.39A1.001 1.001 0 1 1 4.854 3.8a7.431 7.431 0 0 0-1.988 5.037 1 1 0 0 1-1 .995Zm16.268 0a1 1 0 0 1-1-1A7.431 7.431 0 0 0 15.146 3.8a1 1 0 0 1 1.471-1.354 9.425 9.425 0 0 1 2.517 6.391 1 1 0 0 1-1 .995ZM6.823 17a3.453 3.453 0 0 0 6.354 0H6.823Z"/>
                 </svg>
             </div>
-            
-
+            <!-- User Name and User Icon -->
             <div class="flex items-center justify-center ml-2  pl-2 pr-2">
-                    <h3 class="pr-4">Hello, User One!</h3>
-                    <!-- User icon -->
-            <div class="border-2 border-black rounded-full w-12 h-12 pl-2 pr-2 flex items-center justify-center">
-                <svg class="w-6 h-6 dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 18">
-                    <path d="M7 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9Zm2 1H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-                </svg>
+                <h3 class="pr-4">Hello, User One!</h3>
+                <div class="border-2 border-black rounded-full w-12 h-12 pl-2 pr-2 flex items-center justify-center">
+                    <svg class="w-6 h-6 dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 18">
+                        <path d="M7 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9Zm2 1H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
+                    </svg>
+                </div>
             </div>
-
         </div>
+        <!-------------------- Header END ---------------------->
 
-    </div>
-        <div class="pl-4">
-                        <h2 class="text-2xl  font-semibold">Dashboard</h2>
-                        <div class="w-full h-0.5 bg-black"></div> <!-- Horizontal line -->
-
-        <!-- Add content from here - DO NOT TOUCH /\ -->
-        
-            <div class="flex flex-col p-10 w-full">
-                        <h2 class="text-gray-400 text-2xl font-semibold">Overview</h2>
-                        <div class="w-full h-0.5 bg-gray-400"></div> <!-- Horizontal line -->
-            </div>
+        <!-------------------- Page Heading ---------------------->
+        <class="pl-4">
+            <h2 class="text-2xl  font-semibold">Dashboard</h2>
+            <div class="w-full h-0.5 bg-black"></div><!-- Horizontal line -->
+                <!-- Add content from here - DO NOT TOUCH /\ -->
+                <div class="flex flex-col p-10 w-full">
+                    <h2 class="text-gray-400 text-2xl font-semibold">Overview</h2>
+                    <div class="w-full h-0.5 bg-gray-400"></div> <!-- Horizontal line -->
+                    </div>
 
 <!-----------------------------------4 x Squares ------------------------------------------------>
             <div class="flex">
