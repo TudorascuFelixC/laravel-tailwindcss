8ac98178ec06bc24c2eb21b0c1031883591fdd2f
Alberto Mafla Prado <79513200+A0M0305@users.noreply.github.com>
2023-08-31 14:52:19 +0100

    Added note.

--- routes/web.php
+++ routes/web.php
@@ -27,26 +27,6 @@ Route::get('/dashboard-overview', function () {
 
 Route::get('/dashboard-admin', [AdminController::class, 'index'])->name('dashboard-admin');
 
-
-
-Route::get('/main-index', function () {
-    return view('main-index');
-})->name('main-index'); // Define the named route "register" here
-
-Route::get('/subscription', function () {
-    return view('subscription');
-})->name('subscription'); // Define the named route "register" here
-
-Route::get('/test', function () {
-    return view('test');
-})->name('test'); // Define the named route "register" here
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard'); // Define the named route "dashboard" here
-
-
-
 Route::get('/usersubscription', function () {
     return view('usersubscription');
 })->name('usersubscription') -> middleware('auth');
@@ -66,3 +46,21 @@ Route::get('/usersAdmin', function () {
 Route::get('/settings', function () {
     return view('settings');
 })->name('settings')->middleware('auth');
+
+
+// Remove if not needed please. AM.
+Route::get('/main-index', function () {
+    return view('main-index');
+})->name('main-index'); // Define the named route "register" here
+
+Route::get('/subscription', function () {
+    return view('subscription');
+})->name('subscription'); // Define the named route "register" here
+
+Route::get('/test', function () {
+    return view('test');
+})->name('test'); // Define the named route "register" here
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard'); // Define the named route "dashboard" here
